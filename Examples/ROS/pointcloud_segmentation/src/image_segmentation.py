7bd87f0f1b644a4e648677b435c082edcb7dae92
Francisco <francisco.elias@visualbehavior.ai>
2021-04-26 10:02:57 +0200

    Cleanup and documentation of image_segmentation

--- Examples/ROS/pointcloud_segmentation/src/image_segmentation.py
+++ Examples/ROS/pointcloud_segmentation/src/image_segmentation.py
@@ -15,120 +15,145 @@ from detr.DetrPanopticTRT import DetrPanopticTRT
 
 roslib.load_manifest('pointcloud_segmentation')
 
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+
 class ImageSegmentation:
-    def __init__(self):
-        image_sub_topic_name = "/zed/zed_node/rgb/image_rect_color"
-        image_pub_topic_name = "/pointcloud_segmentation/image_mask"
-        engine1_path = "/home/visualbehavior/Documents/surroundnet/weights/detr_panoptic_agv_VGA/detr-panoptic-part1/detr_panoptic_part1_fp16.engine"
-        engine2_path = "/home/visualbehavior/Documents/surroundnet/weights/detr_panoptic_agv_VGA/detr-panoptic-part2/detr_panoptic_part2_fp16.engine"
-        
+    """
+    ROS Node responsible for segmenting incoming images and publishing their masks
+
+    Summary:
+    1. Create a model based on DetrPanopticTRT used for segmenting images 
+    2. Subscribe to a ROS topic, and save incoming images in a buffer
+    3. Segment the images in the buffer using the previous model
+    4. Create a binary mask by combining the generated masks of a single image
+    5. Publish the binary mask to another ROS topic  
+    """
+
+    def __init__(self, image_sub_topic_name, mask_pub_topic_name, engine1_path, engine2_path):
+        """
+        Initialize the ImageSegmentation node and class
+
+        Keyword arguments:
+        image_sub_topic_name -- str, name of the ROS topic to subscribe for the incoming images
+        mask_pub_topic_name -- str, name of the ROS topic to publish the masks
+        engine1_path -- str, path to engine 1 (Backbone + Transformer) for DetrPanopticTRT
+        engine2_path -- str, path to engine 2 (Mask Head) for DetrPanopticTRT
+        """
         image_size = (376, 672, 3)
         self.model = DetrPanopticTRT(engine1_path, engine2_path, image_size)
+
+        rospy.init_node('image_segmentation', anonymous=True)
 
         self.bridge = CvBridge()
         self.images_buffer = []
 
-        print ("Subscribing to\n\t", image_sub_topic_name)
+        print("Subscribing to\n\t", image_sub_topic_name)
         self.image_sub = rospy.Subscriber(
-            image_sub_topic_name, Image, self.callback, queue_size=100)
+            image_sub_topic_name, Image, self._image_received_callback, queue_size=100)
 
-        print ("Publishing to\n\t", image_pub_topic_name)
+        print("Publishing to\n\t", mask_pub_topic_name)
         self.image_pub = rospy.Publisher(
-            image_pub_topic_name, Image, queue_size=100)
+            mask_pub_topic_name, Image, queue_size=100)
 
-        # frame = cv2.imread("/home/visualbehavior/Documents/surroundnet/garage2.png")
-        # mask = self.execute_segmentation(frame)
-        # cv2.imshow("mask", mask)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows()
+    def _image_received_callback(self, data):
+        """
+        Convert image message received from the ROS Node to cv2 image, and add to an image buffer
 
-        # self.images_buffer.append(frame)
-        # self.execute_infer_on_buffer()
-
-    def callback(self, data):
-        print ("Received images")
+        Keyword arguments:
+        data -- ros message containing incoming image
+        """
         try:
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         except CvBridgeError as e:
-            print(e)
+            print("Failed to convert ROS message to cv2 image:", e)
 
-        self.images_buffer.append(cv_image)
-        print("Image Buffer", len(self.images_buffer))
-        # (rows, cols, channels) = cv_image.shape
-        # frame = cv2.imread("/home/visualbehavior/Documents/surroundnet/garage2.jpg")
-        # self.images_buffer.append(frame)
+        self.images_buffer.append((cv_image, data.header.seq))
 
-        # masked_image = self.execute_segmentation(frame)
+    def _combine_masks(self, masks, image):
+        """
+        Combine all masks into one mask and convert it to binary mask
 
-        # masked_image = self.execute_segmentation(cv_image)
+        Keyword arguments:
+        masks -- list of masks
+        image -- input image of the masks
 
+        Return:
+        binary mask
+        """
+        if masks.any():
+            img = sigmoid(masks[:, :, 0]) * 255
 
-    def execute_infer_on_buffer(self):
-        if self.images_buffer:
-            image = self.images_buffer.pop(0)
-            if image is not None:
-                mask = self.execute_segmentation(image)
-                # cv2.imshow("mask", mask)
-                # cv2.waitKey(0)
-                # cv2.destroyAllWindows()
+            for i in range(1, masks.shape[2]):
+                temp_mask = masks[:, :, i]
+                ranged_image = sigmoid(temp_mask) * 255
+                img = cv2.add(img, ranged_image)
+
+            _, binary_mask = cv2.threshold(
+                img, 255./2., 255, cv2.THRESH_BINARY)
+
+            binary_mask = cv2.resize(
+                binary_mask, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_AREA)
+            img = cv2.resize(
+                img, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_AREA)
+            img = np.uint8(binary_mask)
+            # cv2.imshow("binary", img)
+            # cv2.waitKey(3)
+            return img
+        return None
+
+    def generate_segmentation_mask(self, image):
+        """
+        Generate the segmentation of the image using the model,
+        and combine the segmented masks to create one binary mask using _combine_masks(masks)
+
+        Keyword arguments:
+        image -- input image that needs to be segmented
+        """
+        resized_image = cv2.resize(image, (672, 376))
+        self.model.execute(resized_image)
+        boxes, labels, scores, masks = self.model.get_all_output()
+        return self._combine_masks(masks, image)
+
+    def run_once(self):
+        """
+        Segment the first image in the buffer, create it's binary mask,
+        and publish the mask to the relative ROS topic
+        """
+        if not self.images_buffer:
+            return
+
+        image, seq = self.images_buffer.pop(0)
+        if image is not None:
+            mask = self.generate_segmentation_mask(image)
+            if mask is not None and mask.any():
                 try:
-                    self.image_pub.publish(self.bridge.cv2_to_imgmsg(mask, "bgr8"))
-                    print ("Publishing mask")
+                    ros_mask = self.bridge.cv2_to_imgmsg(mask, "mono8")
+                    # The mask should have the same seq number as the image
+                    ros_mask.header.seq = seq
+                    self.image_pub.publish(ros_mask)
+                    print("Publishing mask", ros_mask.header.seq)
+
                 except CvBridgeError as e:
                     print(e)
 
 
-    def execute_segmentation(self, image):
-        print("Original shape of the iamge is", image.shape)
-
-        image = cv2.resize(image, (672, 376))
-        # cv2.imshow("output", image)
-        # cv2.waitKey(0)
-        # cv2.destroyAllWindows() 
-        # print("The shape of the iamge is", image.shape)
-        # print("The shape of the iamge is", image.dtype)
-        self.model.execute(image)
-        boxes, labels, scores, masks = self.model.get_all_output()
- 
-        # Except the first class and the last class, all other classes are for the ground.
-        # You can combine/union all 'ground' mask to have a single mask for the ground 
-        # print("-"*10, " class names ", "-"*10)
-        # print(self.model.CLASS_NAME)
-        # print("-"*10, " bboxes ", "-"*10)
-        # [print(i) for i in boxes]
-        # print("-"*10, " label indexes ", "-"*10)
-        # print(labels)
-        # print("-"*10, " scores ", "-"*10)
-        # print(scores)
-        # print("-"*10, " masks ", "-"*10)
-        # print("masks.shape", masks.shape)
-        # print("masks.max", masks.max())
-        # print("masks.min", masks.min())
-        # we should apply sigmoid to mask to have a score ranging from 0 -> 1 for each pixel
-
-        # If you want data stored in GPU, below variables are in type pycuda.driver.DeviceAllocation
-        # masks -> model.device_masks
-        # boxes -> model.device_out_bbox
-        # raw logit scores -> model.device_out_cls
-
-        # For visualization:
-        output_image = self.model.get_post_processed_image()
-
-        # cv2.imshow("output", output_image)
-        # cv2.waitKey(0)
-        return output_image
-
-
-
-
 def main(args):
-    image_seg = ImageSegmentation()
+    image_sub_topic_name = "/zed/zed_node/rgb/image_rect_color"
+    mask_pub_topic_name = "/pointcloud_segmentation/image_mask"
+    engine1_path = "/home/visualbehavior/Documents/surroundnet/weights/detr_panoptic_agv_VGA/detr-panoptic-part1/detr_panoptic_part1_fp16.engine"
+    engine2_path = "/home/visualbehavior/Documents/surroundnet/weights/detr_panoptic_agv_VGA/detr-panoptic-part2/detr_panoptic_part2_fp16.engine"
 
+    image_seg = ImageSegmentation(image_sub_topic_name, mask_pub_topic_name,
+                                  engine1_path, engine2_path)
 
-    rospy.init_node('image_segmentation', anonymous=True)
+    rate = rospy.Rate(50) # 50Hz
 
     while not rospy.is_shutdown():
-        image_seg.execute_infer_on_buffer()
+        image_seg.run_once()
+        # rate.sleep()
 
     try:
         rospy.spin()
