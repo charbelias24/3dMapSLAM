818311e1907489df995d3b35ee60f433f6fa032d
Francisco <francisco.elias@visualbehavior.ai>
2021-04-29 14:11:43 +0200

    Added building image_segmentation in build_ros.sh

--- build_ros.sh
+++ build_ros.sh
@@ -5,3 +5,9 @@ mkdir build
 cd build
 cmake .. -DROS_BUILD_TYPE=Release
 make 
+
+cd ../../pointcloud_segmentation
+mkdir build
+cd build
+cmake -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.6m -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython3.6m.so ..
+make
